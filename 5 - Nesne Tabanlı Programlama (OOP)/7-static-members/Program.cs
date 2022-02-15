f3026aadb736633bcc3b879b7ebb187d5a5aa3b6
var0sh <akcatepefurkan1@gmail.com>
2022-02-15 13:01:25 +0300

    static-members

---
+++ 5 - Nesne Tabanlı Programlama (OOP)/7-static-members/Program.cs
@@ -0,0 +1,36 @@
+﻿// var s1 = new Student("Furkan", 447);
+// var s2 = new Student("Ayşe", 241);
+// var s3 = new Student("Taha", 352);
+
+// s1.DisplayStudentDetails();
+// Student.DisplaySchoolDetails();
+
+string str = "ölçme ve değerlendirme";
+System.Console.WriteLine(HelperMethods.KarakterDuzelt(str));
+static class HelperMethods {
+    public static string KarakterDuzelt(string str) {
+        return str.Replace("ö", "o")
+            .Replace("ç", "c")
+                .Replace(" ", "-")
+                    .Replace("ğ", "g");
+    }
+}
+class Student {
+
+    public Student(string name, int number) {
+        this.Name = name;
+        this.Number = number;
+    }
+
+    public string Name { get; set; }
+    public int Number { get; set; }
+    static string School = "my school";
+    static string Address = "my school address";
+
+    public void DisplayStudentDetails() {
+        Console.WriteLine($"name: {this.Name} | number: {this.Number} ");
+    }
+    public static void DisplaySchoolDetails() {
+        System.Console.WriteLine($"school name: {Student.School} | school address: {Student.Address}");
+    }
+}
