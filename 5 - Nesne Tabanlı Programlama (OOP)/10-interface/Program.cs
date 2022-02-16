6fb6588ee9bab88ee38dee0fd5216afd7cc1bdc6
var0sh <akcatepefurkan1@gmail.com>
2022-02-16 19:01:06 +0300

    csharp

---
+++ 5 - Nesne Tabanlı Programlama (OOP)/10-interface/Program.cs
@@ -0,0 +1,69 @@
+﻿var personeller = new IPersonel[3];
+
+personeller[0] = new Yonetici("Furkan", "eskişehir", "yazılım");
+personeller[1] = new Robot("bilişim");
+personeller[2] = new Isci("ayşe", "eskişehir", "bilgisayar");
+
+foreach (var item in personeller) {
+    item.bilgi();
+}
+
+interface IPersonel {
+    string departman { get; set; }
+
+    void bilgi();
+}
+interface IKisi {
+    string adSoyad { get; set; }
+    string address { get; set; }
+    string departman { get; set; }
+
+    void bilgi();
+}
+class Yonetici : IPersonel, IKisi {
+
+    public Yonetici(string _adSoyad, string _address, string _departman) {
+        this.adSoyad = _adSoyad;
+        this.address = _address;
+        this.departman = _departman;
+    }
+
+    public string adSoyad { get; set; }
+    public string address { get; set; }
+    public string departman { get; set; }
+    public double maas { get; set; }
+
+    public void bilgi() {
+        System.Console.WriteLine($"{this.adSoyad} isimli personel {this.departman} bölümünde yöneticidir ");
+    }
+}
+class Isci : IPersonel, IKisi {
+
+    public Isci(string _adSoyad, string _address, string _departman) {
+        this.adSoyad = _adSoyad;
+        this.address = _address;
+        this.departman = _departman;
+    }
+
+    public string adSoyad { get; set; }
+    public string address { get; set; }
+    public string departman { get; set; }
+    public double maas { get; set; }
+
+    public void bilgi()
+    {
+        System.Console.WriteLine($"{this.adSoyad} isimli personel {this.departman} bölümünde işçidir");
+    }
+}
+class Robot : IPersonel {
+
+    public Robot(string _departman) {
+        this.departman = _departman;
+    }
+
+    public string departman { get; set; }
+
+    public void bilgi() {
+        System.Console.WriteLine($"{this.departman} bölümünde bir robot");
+    }
+}
---
+++ 5 - Nesne Tabanlı Programlama (OOP)/9-abstract-siniflar/Program.cs
@@ -0,0 +1,79 @@
+﻿var shapes = new Shape[3];
+
+shapes[0] = new Diktortgen(10, 3);
+shapes[1] = new Kare(5, 7);
+shapes[2] = new Diktortgen(3, 9);
+
+foreach (var item in shapes) {
+    item.Draw();
+    System.Console.WriteLine(item.CalculateArea());
+}
+
+abstract class Person {
+    
+    public Person (string name, string surName) {
+        this.Name = name;
+        this.SurName = surName;
+        System.Console.WriteLine("Person nesnesi oluşturuldu");
+    }
+    
+    public string Name { get; set; }
+    public string SurName { get; set; }
+
+    public abstract void Intro();
+    public void Greeting() {
+        System.Console.WriteLine("i am a person");
+    }
+
+}
+class Student: Person {
+
+    public Student (string name, string surName, string number): base(name, surName) {
+        this.Number = number;
+        System.Console.WriteLine("Student nesnesi oluşturuldu");
+    }
+    public string Number { get; set; }
+
+    public override void Intro() {
+        System.Console.WriteLine($"name: {this.Name} | surname: {this.SurName} | number: {this.Number} ");
+    }
+
+}
+class Teacher: Person {
+    public Teacher (string name, string surName, string branch): base(name, surName) {
+        this.Branch = branch;
+    }
+    public string Branch { get; set; }
+
+    public void Teac() {
+        System.Console.WriteLine("i am teaching..");
+    }
+    public override void Intro() {
+        System.Console.WriteLine($"name: {this.Name} | surname: {this.SurName} | branch: {this.Branch} ");
+    }
+}
+abstract class Shape {
+    public Shape(int w, int h) {
+        this.Width = w;
+        this.Height = h;
+    }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public int CalculateArea() {
+        return this.Width * this.Height;
+    }
+    public abstract void Draw();
+}
+class Kare: Shape {
+    public Kare(int w, int h): base(w, h) {}
+    public override void Draw() {
+        System.Console.WriteLine("draw a square");
+    }
+}
+class Diktortgen: Shape {
+    public Diktortgen(int w, int h): base(w, h) {}
+    public override void Draw() {
+        System.Console.WriteLine("draw a rectangle");
+    }
+}
