a6f9259cc8443bb2270ba6c107f1f1c1019776e0
var0sh <akcatepefurkan1@gmail.com>
2022-02-17 23:20:31 +0300

    custom-exception

---
+++ 7 - Hata Yönetimi/4-custom-exception/Program.cs
@@ -0,0 +1,17 @@
+﻿try {
+    Login("furkan", "456");
+    System.Console.WriteLine("login başarılı");
+} catch (LoginException err) {
+    System.Console.WriteLine(err.Message);
+}
+
+static void Login (string username, string password) {
+    if (username.Contains(" "))
+        throw new LoginException("username boşluk içeremez");
+    else if(password.Length < 8)
+        throw new LoginException("parola min 8 karakter olmalıdır");
+}
+
+class LoginException: Exception {
+    public LoginException(string message): base(message) {}
+}
