c120b438f61e04556a69a2479fe95af1c666ad7a
var0sh <akcatepefurkan1@gmail.com>
2022-02-17 18:48:59 +0300

    hata-yonetimi

---
+++ 7 - Hata Yönetimi/2-hata-yonetimi/Program.cs
@@ -0,0 +1,31 @@
+﻿// **Unhandled exception. System.FormatException
+// **Unhandled exception. System.DivideByZeroException
+// **Unhandled exception. System.IndexOutOfRangeException
+// **Unhandled exception. System.NullReferenceException
+
+try {
+    System.Console.Write("a: ");
+    int a = int.Parse(Console.ReadLine());
+    
+    System.Console.Write("b: ");
+    int b = int.Parse(Console.ReadLine());
+    
+    var sonuc = a / b;
+    
+    System.Console.WriteLine($"{a} / {b} = {sonuc}");
+} 
+// catch (System.DivideByZeroException error) {
+//     System.Console.WriteLine("b 0 olamaz");
+//     System.Console.WriteLine(error.Message);
+// } 
+// catch (FormatException error) {
+//     System.Console.WriteLine("sayısal bilgi girmelisiniz");
+//     System.Console.WriteLine(error.Message);
+// } 
+catch (Exception err) {
+    System.Console.WriteLine("Bir hata oluştu.");
+    System.Console.WriteLine(err.Message);
+}
+finally {
+    System.Console.WriteLine("finally bloğu çalıştı");
+}
