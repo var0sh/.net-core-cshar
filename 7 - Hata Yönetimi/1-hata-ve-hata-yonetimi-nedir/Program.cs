71133b3e23e7763e4a4c9c2dcadbd1e1b47f4629
var0sh <akcatepefurkan1@gmail.com>
2022-02-17 18:26:12 +0300

    hata-ve-hata-yonetimi-nedir

---
+++ 7 - Hata Yönetimi/1-hata-ve-hata-yonetimi-nedir/Program.cs
@@ -0,0 +1,22 @@
+﻿// **Exception
+
+// **Unhandled exception. System.FormatException
+// **Unhandled exception. System.DivideByZeroException
+// **Unhandled exception. System.IndexOutOfRangeException
+// **Unhandled exception. System.NullReferenceException
+
+System.Console.Write("sayı 1: ");
+var sayi1 = int.Parse(Console.ReadLine());
+System.Console.Write("sayı 2: ");
+var sayi2 = int.Parse(Console.ReadLine());
+int[] sayilar = new int[2];
+// sayilar[2] = 10;
+
+var sonuc = sayi1 / sayi2;
+
+Product p = null;
+System.Console.WriteLine(p.Name);
+
+class Product {
+    public string Name { get; set; }
+}
