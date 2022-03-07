46117499ca8b3c4305922000eb738fdc9541d3fa
var0sh <akcatepefurkan1@gmail.com>
2022-03-07 18:35:16 +0300

    1-veri-tabani-baglantisinin-kurulmasi

---
+++ 8 - Veri Tabanı İle Çalışma/1-veri-tabani-baglantisinin-kurulmasi-sql-server/Program.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+
+GetMySqlConnection();
+
+static void GetMySqlConnection() {
+    string connectionString = @"server=localhost;port=3306;database=northwind;user=root;password=12345;";
+
+    using (var connection = new MySqlConnection(connectionString)) { 
+        try {
+            connection.Open();
+            System.Console.WriteLine("bağlantı sağlandı");
+        } catch (Exception err) {
+            System.Console.WriteLine(err.Message);
+        } finally {
+            connection.Close();
+        }
+    }
+}
